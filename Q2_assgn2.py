947e6406c7ca2701fe970fc44c13f08a8bd72065
BT22103117 <121217470+BT22103117@users.noreply.github.com>
2022-12-22 21:53:28 +0530

    Add files via upload

---
+++ Q2_assgn2.py
@@ -0,0 +1,5 @@
+name = "avtar singh"
+SID = "22103117"
+department = "computer science engineering"
+CGPA = 9.0
+print("Hey,", name, "Here!\nMy SID is", SID, "\nI am from", department, "department and my CGPA is", CGPA)
