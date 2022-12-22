a932d756b60ffcb1756195bacd3b22b352e1d9c8
BT22103117 <121217470+BT22103117@users.noreply.github.com>
2022-12-22 12:04:11 +0530

    Add files via upload

---
+++ average Q1 assgn 1.py
@@ -0,0 +1,5 @@
+a=input("enter number 1 ")
+b=input("enter number 2 ")
+c=input("enter number 3 ")
+average=(int(a)+int(b)+int(c))/3
+print(average)
---
+++ second Q3 assgn 1.py
@@ -0,0 +1,8 @@
+seconds=int(input("enter number of the seconds = "))
+minutes=int(seconds/60)
+sec=(seconds%60)
+
+print("minutes =  ",end='')
+print(minutes)
+print("sec = ",end='')
+print(sec)
